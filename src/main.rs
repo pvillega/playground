9dd2c459df0932a2e9de0b11562693c93dd4fdc2
Pere Villega <pvillega@aracon.com>
2021-03-10 18:43:23 +0000

    Add code from rust-by-example
    Signed-off-by: Pere Villega <pvillega@aracon.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+#[derive(Debug)]
+struct Person<'a> {
+    name: &'a str,
+    age: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let name = "Peter";
+    let age = 27;
+    let peter = Person { name, age };
+
+    // Pretty print
+    println!("{:#?}", peter);
+
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is: {}", secret_number);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess: String = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break
+            },
+        }
+    }
 }
